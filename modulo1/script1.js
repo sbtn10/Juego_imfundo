f3223a718a852c2f42de7e3a9b80d978cc45385d
sbtn10 <sebastianb1208@gmail.com>
2024-06-06 14:21:41 -0500

    v

--- modulo1/script1.js
+++ modulo1/script1.js
@@ -1,51 +1,34 @@
-const muteIcon = document.querySelector('#mute-icon');
-const soundIcon = document.querySelector('#sound-icon');
-var audio = document.querySelector('#audioElement');
-audio.volume = 0.03;
-
-let isMuted = false;
-
-soundIcon.addEventListener('click', () => {
-    audio.volume = isMuted ? 0.03 : 0;
-    isMuted = !isMuted;
-    const icono = document.getElementById("mute-icon");
-    const icon = document.getElementById("sound-icon");
-    icon.style.display = 'none';
-    icono.style.display = 'block';
-});
-
-muteIcon.addEventListener('click', () => {
-    audio.volume = isMuted ? 0.03 : 0;
-    isMuted = !isMuted;
-    const icono = document.getElementById("mute-icon");
-    const icon = document.getElementById("sound-icon");
-    icon.style.display = 'block';
-    icono.style.display = 'none';
-});
-
 document.addEventListener('DOMContentLoaded', function () {
-    const cards = document.querySelectorAll('.card');
-    const words = document.querySelectorAll('.word');
+    const muteIcon = document.querySelector('#mute-icon');
+    const soundIcon = document.querySelector('#sound-icon');
+    const audio = document.querySelector('#audioElement');
     const correctMessage = document.getElementById('correct-message');
     const errorMessage = document.getElementById('error-message');
     const completedMessage = document.getElementById('completed');
     const modalGameOver = document.getElementById('gameOverModal');
+    const hearts = document.querySelectorAll('.heart');
+    const cards = document.querySelectorAll('.card');
+    const words = document.querySelectorAll('.word');
+    const reintentarBtn = document.getElementById('reintentarBtn');
+    const salirBtn = document.getElementById('salirBtn');
 
-    // Sonidos
     const audioCorrecto = new Audio('../audio/correcto.mp3');
     const audioIncorrecto = new Audio('../audio/incorrecto.mp3');
     const audioGameOver = new Audio('..audio/gameover.mp3');
+    audio.volume = 0.03;
+    audioGameOver.volume = 0.5;
 
+    let isMuted = false;
     let selectedCard = null;
     let selectedWord = null;
     let attempts = 0;
     const maxAttempts = 3;
     let matchedPairs = 0;
     const totalPairs = cards.length / 2;
-    let lives = 3; // Número inicial de vidas
+    let lives = 3;
 
-    // Selector de corazones
-    const hearts = document.querySelectorAll('.heart');
+    soundIcon.addEventListener('click', toggleSound);
+    muteIcon.addEventListener('click', toggleSound);
 
     cards.forEach(card => {
         card.addEventListener('click', selectCard);
@@ -54,6 +37,16 @@ document.addEventListener('DOMContentLoaded', function () {
     words.forEach(word => {
         word.addEventListener('click', selectWord);
     });
+
+    reintentarBtn.addEventListener('click', () => window.location.href = 'game3.html');
+    salirBtn.addEventListener('click', () => window.location.href = '../index.html');
+
+    function toggleSound() {
+        audio.volume = isMuted ? 0.03 : 0;
+        isMuted = !isMuted;
+        soundIcon.style.display = isMuted ? 'none' : 'block';
+        muteIcon.style.display = isMuted ? 'block' : 'none';
+    }
 
     function selectCard() {
         if (selectedCard === null || this !== selectedCard) {
@@ -89,40 +82,43 @@ document.addEventListener('DOMContentLoaded', function () {
         selectedCard = null;
         selectedWord = null;
         matchedPairs++;
-        correctMessage.style.display = 'block';
-        playAudio(audioCorrecto);
-        setTimeout(function () {
-            correctMessage.style.display = 'none';
-        }, 1000);
-
+        showMessage(correctMessage, audioCorrecto);
+    
         if (matchedPairs === totalPairs) {
             completedMessage.style.display = 'block';
-
         }
-
+    
         // Verificar si todas las cartas están deshabilitadas
         const allDisabled = Array.from(cards).every(card => card.classList.contains('destroyed'));
-
+    
         if (allDisabled) {
             // Todas las cartas están deshabilitadas, mostrar la flecha
             const arrowIcon = document.getElementById('arrow-icon');
             const modal = document.getElementById("modal");
-            modal.style.display="flex";
-            modal.classList.add("show");
-            arrowIcon.style.display = 'inline-block'; // Cambia 'inline-block' según el estilo de la flecha en tu CSS
-            arrowIcon.addEventListener('click', function() {
-                window.location.href = 'game2.html';
-            });
-            setTimeout(() => {
-                modal.classList.remove("show");
-                modal.classList.add("hide");
+            if (modal) {  // Verificación adicional
+                modal.style.display = "flex";
+                modal.classList.add("show");
+                arrowIcon.style.display = 'inline-block';
+                arrowIcon.addEventListener('click', function() {
+                    window.location.href = '../final.html';
+                });
+    
+                // Ocultar el modal después de 10 segundos
                 setTimeout(() => {
-                    modal.style.display = "none";
-                    modal.classList.remove("hide");
-                }, 500); 
-            }, 1200); 
+                    modal.classList.remove("show");
+                    modal.classList.add("hide");
+                    setTimeout(() => {
+                        modal.style.display = "none";
+                        modal.classList.remove("hide");
+                    }, 500); 
+                }, 1000); // 10000 milisegundos = 10 segundos
+            } else {
+                console.error('El elemento con el ID "modal" no existe en el DOM.');
+            }
         }
     }
+    
+    
 
     function incorrectMatch() {
         selectedCard.classList.add('incorrect');
@@ -133,70 +129,41 @@ document.addEventListener('DOMContentLoaded', function () {
         selectedCard = null;
         selectedWord = null;
         attempts++;
-        errorMessage.style.display = 'block';
-        playAudio(audioIncorrecto);
-        setTimeout(function () {
-            errorMessage.style.display = 'none';
-        }, 1000);
+        showMessage(errorMessage, audioIncorrecto);
 
-        // Reducir el número de vidas y animar la desaparición de un corazón
         lives--;
         if (lives >= 0) {
-            animateHeartDisappearance(hearts[lives]); // Animar la desaparición del corazón correspondiente
+            animateHeartDisappearance(hearts[lives]);
         }
 
         if (attempts === maxAttempts) {
-            setTimeout(function () {
-                mostrarGameOver();
-            }, 500);
+            setTimeout(mostrarGameOver, 500);
         }
     }
 
     function mostrarGameOver() {
         modalGameOver.style.display = 'block';
-
-        const reintentarBtn = document.getElementById('reintentarBtn');
-        const salirBtn = document.getElementById('salirBtn');
-
-        reintentarBtn.addEventListener('click', function () {
-            modalGameOver.style.display = 'none';
-            window.location.href = 'game1.html'
-        });
-
-        salirBtn.addEventListener('click', function () {
-            modalGameOver.style.display = 'none';
-            window.location.href = '../index.html'
-        });
-
-        audioGameOver.volume = 0.5;
         audioGameOver.play();
     }
 
     function animateHeartDisappearance(heart) {
-        heart.style.transition = 'opacity 0.5s ease-out'; // Agrega una transición para el efecto de desvanecimiento
-        heart.style.opacity = '0'; // Reduce gradualmente la opacidad del corazón hasta que desaparezca
+        heart.style.transition = 'opacity 0.5s ease-out';
+        heart.style.opacity = '0';
         setTimeout(() => {
-            heart.style.display = 'none'; // Oculta el corazón después de que termine la animación
-        }, 500); // La duración de la transición es de 0.5 segundos
+            heart.style.display = 'none';
+        }, 500);
     }
 
-    var closeSVG = document.querySelector(".close-svg");
-
-    // Función para manejar el clic en el SVG de cierre
-    function handleCloseClick() {
-        // Redirigir a la página de inicio
-        window.location.href = "https://www.ejemplo.com";
+    function showMessage(messageElement, audio) {
+        messageElement.style.display = 'block';
+        playAudio(audio);
+        setTimeout(() => {
+            messageElement.style.display = 'none';
+        }, 1000);
     }
-
 
     function playAudio(audio) {
-        if (audio.paused) {
-            audio.currentTime = 0;
-            audio.play();
-        } else {
-            audio.pause();
-            audio.currentTime = 0;
-            audio.play();
-        }
+        audio.currentTime = 0;
+        audio.play();
     }
 });
